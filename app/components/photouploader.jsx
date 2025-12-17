a0347c780243a314bdddb44e64750e43f9ff8a0e
ademhilmibozkurt <bozkurtadem7@gmail.com>
2025-12-17 11:00:03 +0300

    fix/Revalidate
    Revalidating api endpoint added for page refresh after uploading images. Cache refreshed.

--- app/components/photouploader.jsx
+++ app/components/photouploader.jsx
@@ -33,6 +33,12 @@ export default function PhotoUploader()
                 throw error
             }
 
+            await fetch('/api/revalidate', 
+                {
+                    method:'POST',
+                    headers: { 'Content-Type': 'application/json' },
+                    body: JSON.stringify({path: '/photos'})
+                })
             router.refresh()
         }
         catch(err)
