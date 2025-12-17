728ca2d6bcbeba93eb04dd165642b15a2758e1de
ademhilmibozkurt <bozkurtadem7@gmail.com>
2025-12-17 11:48:05 +0300

    feature/PhotoModal
    Photos now can see in pop up page.

--- app/components/Photo.jsx
+++ app/components/Photo.jsx
@@ -1,18 +1,33 @@
 'use client'
 
 import Image from "next/image"
+import { useState } from "react"
+import PhotoModal from "./PhotoModal"
 
 export default function Photo({src, alt, width, height, photoName, isFavorited=false})
 {
+    const [showModal, setShowModal] = useState(false)
+
+    function toggleModal()
+    {
+        setShowModal(!showModal)
+    }
+
     return (
-        <div 
-            style={{width, height}}
-            className="relative w-auto h-auto shadow-md border border-white border-opacity-80 rounded-lg overflow-hidden cursor-pointer">
-                <Image 
-                    src={src}
-                    alt={alt}
-                    layout="fill"
-                    style={{objectFit: 'cover', objectPosition: 'center'}}/>
-        </div>
+        <>
+            <div 
+                style={{width, height}}
+                className="relative w-auto h-auto shadow-md border border-white border-opacity-80 rounded-lg overflow-hidden cursor-pointer">
+                    <Image 
+                        src={src}
+                        alt={alt}
+                        layout="fill"
+                        style={{objectFit: 'cover', objectPosition: 'center'}}
+                        onClick={() => setShowModal(true)}/>
+            </div>
+            {
+                showModal && <PhotoModal src={src} alt={alt} onClose={toggleModal}/>
+            }
+        </>
     )
 }
